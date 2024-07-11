ab700a0ef0ad4021e44e3873b16b1ba83226d0b0
Rohan Singh <rs7289579@gmail.com>
2024-07-11 09:53:48 +0530

    UI - Created popular tab

---
+++ lib/components/popular_tab.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:food_panda/data/data.dart';
+
+class PopularTab extends StatelessWidget {
+  const PopularTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              "Popular",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text("Most ordered right now"),
+            const SizedBox(
+              height: 12,
+            ),
+            StaggeredGrid.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+                children: Data.popularItems.map((item) {
+                  return StaggeredGridTile.count(
+                      crossAxisCellCount: 1,
+                      mainAxisCellCount: 1,
+                      child: Container(
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                spreadRadius: 4,
+                                blurRadius: 6,
+                              ),
+                            ]),
+                        child: Stack(
+                          children: [
+                            Positioned.fill(
+                              child: Image.asset(
+                                item['image']!,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topCenter,
+                              child: Container(
+                                width: double.infinity,
+                                padding: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                  colors: [
+                                    Colors.black.withOpacity(0.5),
+                                    Colors.black.withOpacity(0.3),
+                                    Colors.transparent
+                                  ],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                )),
+                                child: Text(
+                                  item['title']!,
+                                  style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                                bottom: 12,
+                                right: 8,
+                                child: Container(
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: Row(
+                                    children: <Widget>[
+                                      if (item['oldPrice'] != null)
+                                        Text(
+                                          item['oldPrice']!,
+                                          style: const TextStyle(
+                                              decoration:
+                                                  TextDecoration.lineThrough,
+                                              color: Colors.grey),
+                                        ),
+                                      if (item['oldPrice'] != null)
+                                        const SizedBox(
+                                          width: 12,
+                                        ),
+                                      Text(
+                                        item['price']!,
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      )
+                                    ],
+                                  ),
+                                ))
+                          ],
+                        ),
+                      ));
+                }).toList() as List<Widget>)
+          ]),
+    );
+  }
+}
--- lib/screens/shops_screen.dart
+++ lib/screens/shops_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_panda/components/popular_tab.dart';
 import 'package:food_panda/data/data.dart';
 
 class ShopsScreen extends StatefulWidget {
@@ -86,159 +87,187 @@ class _ShopsScreenState extends State<ShopsScreen>
               ),
             ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.all(6),
-                child: Row(children: <Widget>[
-                  CircleAvatar(
-                    radius: 34,
-                    backgroundImage: AssetImage('assets/subway_logo.jpg'),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                    "Subway",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  )
-                ]),
-              ),
-              const SizedBox(
-                height: 6,
-              ),
-              const Row(children: <Widget>[
-                Expanded(
-                  child: Text(
-                    "1.5km away | Rs. 99.00 Delivery | Rs. 249.00 Minimum Service fee applied",
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                ),
-                SizedBox(
-                  width: 16,
-                ),
-                Text(
-                  "More info",
-                  style: TextStyle(
-                      color: Color(0xFFFF2B85), fontWeight: FontWeight.bold),
-                )
-              ]),
-              const SizedBox(
-                height: 20,
-              ),
-              const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(children: <Widget>[
-                      Icon(
-                        Icons.star_outline_outlined,
-                        color: Color(0xFFFF2B85),
-                      ),
-                      SizedBox(
-                        width: 8,
-                      ),
-                      Text(
-                        "3.9",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        width: 8,
-                      ),
-                      Text(
-                        "1,000+ Ratings",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ]),
-                    Text(
-                      "See reviews",
-                      style: TextStyle(
-                          color: Color(0xFFFF2B85),
-                          fontWeight: FontWeight.bold),
-                    )
-                  ]),
-              const SizedBox(
-                height: 16,
-              ),
-              const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(children: <Widget>[
-                      Icon(
-                        CupertinoIcons.clock,
-                        color: Color(0xFFFF2B85),
-                      ),
-                      SizedBox(
-                        width: 8,
-                      ),
-                      Text(
-                        "Delivery: 15-30 min",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ]),
-                    Text(
-                      "Change",
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    )
-                  ]),
-              const SizedBox(
-                height: 16,
-              ),
-              Row(children: <Widget>[
-                const SizedBox(
-                  width: 4,
-                ),
-                Image.asset(
-                  "assets/voucher_panda.png",
-                  width: 20,
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                const Text(
-                  "Available deals",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ]),
-              const SizedBox(
-                height: 6,
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 12),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-                decoration: BoxDecoration(
-                    color: const Color(0xFFFF2B85).withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(12)),
-                child: Column(children: <Widget>[
-                  Row(children: <Widget>[
-                    Image.asset(
-                      "assets/percentage_icon.png",
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.all(6),
+                  child: Row(children: <Widget>[
+                    CircleAvatar(
+                      radius: 34,
+                      backgroundImage: AssetImage('assets/subway_logo.jpg'),
+                    ),
+                    SizedBox(
                       width: 20,
                     ),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    const Text(
-                      "Rs. 320 off",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFFFF2B85)),
+                    Text(
+                      "Subway",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     )
                   ]),
-                  const SizedBox(
-                    height: 8,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                const Row(children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      "1.5km away | Rs. 99.00 Delivery | Rs. 249.00 Minimum Service fee applied",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
                   ),
                   Text(
-                    "Min order Rs. 320. Valid for Selected  items Auto-applied",
-                    softWrap: true,
-                    style: TextStyle(color: Colors.grey[700]),
+                    "More info",
+                    style: TextStyle(
+                        color: Color(0xFFFF2B85), fontWeight: FontWeight.bold),
                   )
                 ]),
-              )
-            ]),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(children: <Widget>[
+                        Icon(
+                          Icons.star_outline_outlined,
+                          color: Color(0xFFFF2B85),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "3.9",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "1,000+ Ratings",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ]),
+                      Text(
+                        "See reviews",
+                        style: TextStyle(
+                            color: Color(0xFFFF2B85),
+                            fontWeight: FontWeight.bold),
+                      )
+                    ]),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(children: <Widget>[
+                        Icon(
+                          CupertinoIcons.clock,
+                          color: Color(0xFFFF2B85),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "Delivery: 15-30 min",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ]),
+                      Text(
+                        "Change",
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      )
+                    ]),
+                const SizedBox(
+                  height: 16,
+                ),
+                Row(children: <Widget>[
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  Image.asset(
+                    "assets/voucher_panda.png",
+                    width: 20,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text(
+                    "Available deals",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ]),
+                const SizedBox(
+                  height: 6,
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 12),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                  decoration: BoxDecoration(
+                      color: const Color(0xFFFF2B85).withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Column(children: <Widget>[
+                    Row(children: <Widget>[
+                      Image.asset(
+                        "assets/percentage_icon.png",
+                        width: 20,
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      const Text(
+                        "Rs. 320 off",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFFF2B85)),
+                      )
+                    ]),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Min order Rs. 320. Valid for Selected  items Auto-applied",
+                      softWrap: true,
+                      style: TextStyle(color: Colors.grey[700]),
+                    )
+                  ]),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TabBar(
+                    tabAlignment: TabAlignment.start,
+                    controller: _tabController,
+                    isScrollable: true,
+                    labelColor: const Color(0xFFFF2B85),
+                    indicatorColor: const Color(0xFFFF2B85),
+                    labelStyle: const TextStyle(fontWeight: FontWeight.bold),
+                    unselectedLabelColor: Colors.black,
+                    indicatorWeight: 3,
+                    tabs: Data.categories
+                        .map(
+                          (e) => Tab(text: e),
+                        )
+                        .toList()),
+                Container(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: [
+                      const PopularTab(),
+                      Text("Creacket"),
+                      Text("Pepsi"),
+                      Text("All")
+                    ][_tabController.index])
+              ]),
+            ),
           ),
         ));
   }
